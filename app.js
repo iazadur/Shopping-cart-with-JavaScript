71b934fa811a2268ff6e08adabf41ada14c10c3d
iazadur <iamazadur@gmail.com>
2021-08-18 00:33:33 +0600

    all update

--- app.js
+++ app.js
@@ -12,7 +12,28 @@ let updateProductNumber = function (product, price, isIncrement) {
     // update Product total
     const productTotal = document.getElementById(product + '-total')
     productTotal.innerText = productNumber * price
+    calculateTotal()
 }
+
+
+let getInputValue = function (product, price) {
+    const productInput = document.getElementById(product + '-number')
+    let productNumber = productInput.value
+    return productNumber * price
+}
+
+
+let calculateTotal = function () {
+    const phoneTotal = getInputValue('phone', 1219)
+    const caseTotal = getInputValue('case', 59)
+    const subtotal = phoneTotal + caseTotal
+    const tax = subtotal / 10 // 10% vat calculation
+    const totalPrice = subtotal + tax
+    document.getElementById('sub-total').innerText = subtotal
+    document.getElementById('tax-ammount').innerText = tax
+    document.getElementById('total-price').innerText = totalPrice
+}
+
 
 // handle Phone
 document.getElementById('phone-plus').addEventListener('click', function () {

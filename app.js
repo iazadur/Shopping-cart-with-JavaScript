7749e576d4898fb67de441255584b094664acdbf
iazadur <iamazadur@gmail.com>
2021-08-18 00:03:14 +0600

    increment decrementupdated

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log('I am in a separate file')
+let updateProductNumber = function (product, price, isIncrement) {
+    const productInput = document.getElementById(product + '-number')
+    let productNumber = productInput.value
+    if (isIncrement) {
+        productNumber = parseInt(productNumber) + 1
+    } else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1
+
+    }
+    productInput.value = productNumber
+
+    // update Product total
+    const productTotal = document.getElementById(product + '-total')
+    productTotal.innerText = productNumber * price
+}
+
+// handle Phone
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, true)
+})
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, false)
+})
+
+// handle case
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateProductNumber('case', 59, true)
+})
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateProductNumber('case', 59, false)
+})
